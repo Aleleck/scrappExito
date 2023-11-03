e9aed091c05f31e7fb70b1e07f069bc18979f576
Aleleck <panchaponal18@gmail.com>
2023-11-03 17:11:56 -0500

    asdfasdf

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 #"cerveza, aguardiente, ron, whisky"=https://www.exito.com/mercado/vinos-y-licores/aguardiente/cervezas/ron/whisky?fuzzy=0&initialMap=c,c&initialQuery=mercado/vinos-y-licores&map=category-1,category-2,category-3,category-3,category-3,category-3&operator=and&order=OrderByBestDiscountDESC&page=
-#""
+#"sadfsadfasdfs"
 #
